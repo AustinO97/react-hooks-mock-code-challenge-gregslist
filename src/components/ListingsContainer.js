a08345db2242bbb38e47603baa35ce4d150c92ba
Austin Oblow <austinoblow97@gmail.com>
2023-11-15 18:23:52 -0600

    completed lab

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,42 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([])
+
+
+  const filteredListings = listings.filter((listing) => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+
+    useEffect(() => {
+      fetch('http://localhost:6001/listings')
+      .then(res => res.json())
+      .then(listings => setListings(listings))
+    }, [])
+
+
+
+  const newListingCard = filteredListings.map((data) => {
+    return (
+    <ListingCard 
+    id={data.id}
+    key={data.id} 
+    description={data.description}
+    image={data.image}
+    location={data.location}
+    setListings={setListings}
+    listings={listings}
+    />
+    )
+  })
+
+  console.log();
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {newListingCard}
       </ul>
     </main>
   );
